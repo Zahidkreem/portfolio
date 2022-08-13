daae367bdcc0dcbcbfca6eab4ffa01b456209b08
zahidkreem <politepious@gmail.com>
2022-08-13 22:24:23 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+function App() {
+  return (
+    <div className="App">
+        <h2>portfolio project</h2>
+    </div>
+  );
+}
+
+export default App;
